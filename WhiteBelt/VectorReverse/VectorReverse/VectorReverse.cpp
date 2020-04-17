68fb73a1e863a689e0d7a6c9c39e262bcd3dba54
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2020-04-17 21:13:39 +0300

    Added Vector reverse task

---
+++ WhiteBelt/VectorReverse/VectorReverse/VectorReverse.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void Reverse(vector<int>& v);
+
+int main()
+{
+    vector<int> numbers = { 1, 5, 3, 4, 2 };
+    Reverse(numbers);
+    return 0;
+}
+
+void Reverse(vector<int>& v)
+{
+    reverse(v.begin(), v.end());
+}
